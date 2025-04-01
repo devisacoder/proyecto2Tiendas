b909cc9cb62ab0950db11835e7f6ff728a6a122f
Devisacoder <devisa.coder@gmail.com>
2025-04-01 12:23:04 -0500

    maquetacion del header

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react'
 import userIcon from '../../assets/userIcon.svg' 
 import './users.css'
 
 export const Users = () => {
+    const [ open, setOpen ] = useState(false)
+
+    const handleClick = () => {
+        setOpen(prev => !prev)
+    }
+
     return(
-        <img src={userIcon}  className="img-user" alt="icono del usuario"/>
+        <div>
+
+            <img src={userIcon} onClick={handleClick}  className="img-user" alt="icono del usuario"/>
+        
+            {open && (
+                <div className='container-loug-out'>
+                    <ul>
+                        <li className='link-logn-out'>Log out</li>
+                    </ul>
+                </div>
+            )}
+        </div>
     )
 }
