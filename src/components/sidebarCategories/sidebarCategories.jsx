d234cc578d273c4c7f4a0458e5bc3f362be7eac8
Devisacoder <devisa.coder@gmail.com>
2025-04-14 16:49:22 -0500

    ++++

--- src/components/sidebarCategories/sidebarCategories.jsx
+++ src/components/sidebarCategories/sidebarCategories.jsx
@@ -1,32 +1,43 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { useCategories } from '../../hooks/useCategories'
 import { Search } from '../search/search'
 import './sidebarCategories.css'
 import { CategoryContext } from '../../context/Categories'
 
 export const SidebarCategories = () => {
-    const { uniqueCategories, handleClick  } = useCategories()
-    const { selected } = useContext(CategoryContext)
-    
-    return(
-        <div className="container-sidebar">
-            <h2 className='title-sidebar'>Categories</h2>
-            <div className='search-sidebar'>
-                <Search/>
-            </div>
-            <div className="scrollable-categories">
-                <ul className="ul-sidebar">
-                    {uniqueCategories.map((cat) => (
-                        <li 
-                            key={cat}
-                            onClick={() => handleClick(cat)}
-                            className={selected === cat ? 'active-category' : ''}
-                        >
-                            {cat}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const { uniqueCategories, handleClick } = useCategories()
+  const { selected } = useContext(CategoryContext)
+
+  const handleToggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
+
+  return (
+    <div className={`container-sidebar ${isSidebarOpen ? 'open' : ''}`}>
+      <h2 className="title-sidebar" onClick={handleToggleSidebar}>
+        Categories
+      </h2>
+      
+      {/* Usamos un contenedor extra para el contenido del sidebar */}
+      <div className="sidebar-content">
+        <div className="search-sidebar">
+          <Search />
         </div>
-    )
+        <div className="scrollable-categories">
+          <ul className="ul-sidebar">
+            {uniqueCategories.map((cat) => (
+              <li 
+                key={cat}
+                onClick={() => handleClick(cat)}
+                className={selected === cat ? 'active-category' : ''}
+              >
+                {cat}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
